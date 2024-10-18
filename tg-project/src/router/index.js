317457ea816719037ffbcf0fa77556f7c0765dfe
ArkisOnet <yasmin200113@gmail.com>
2024-10-18 15:16:07 +0500

    seven

--- tg-project/src/router/index.js
+++ tg-project/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@/views/Home.vue';
+import HomePage from '@/views/HomePage.vue';
 import TalentMarketplace from '@/components/TalentMarketplace.vue';
 import OutsourcingEOR from '@/components/OutsourcingEOR.vue';
 import TalentPoolManagement from '@/components/TalentPoolManagement.vue';
@@ -10,7 +10,7 @@ Vue.use(Router);
 export default new Router({
     mode: 'history',
     routes: [
-        { path: '/', component: Home },
+        { path: '/', component: HomePage },
         { path: '/marketplace', component: TalentMarketplace },
         { path: '/outsourcing', component: OutsourcingEOR },
         { path: '/talent-pool', component: TalentPoolManagement }
